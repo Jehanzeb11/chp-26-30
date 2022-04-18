c9e232e560cc480cde58cad779170b21879e8c0f
jehanzeb <jehanzebsiddiqui287@gmail.com>
2022-04-19 03:49:03 +0500

    chp 26 to 30 done

---
+++ chp 26 - 30/app.js
@@ -0,0 +1,250 @@
+// question 1
+
+
+// let inp = +prompt("enter number")
+
+// document.write(inp + "<br>")
+
+// document.write("math round : " +Math.round(inp)+ "<br>")
+// document.write("math floor : " +Math.floor(inp)+ "<br>")
+// document.write("math ceil : " +Math.ceil(inp)+ "<br>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 2
+
+
+// let inp = +prompt("enter number")
+
+// document.write(inp + "<br>")
+
+// document.write("math round : " +Math.round(inp)+ "<br>")
+// document.write("math floor : " +Math.floor(inp)+ "<br>")
+// document.write("math ceil : " +Math.ceil(inp)+ "<br>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 3
+
+// let abs = +prompt("enter absolute number")
+
+
+// document.write("absolute value of "+abs+" is "+Math.abs(abs))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 4
+
+// let diceRoll = Math.floor( Math.random() * 6 ) +1;
+// document.write('Random dice value: ' + diceRoll);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 5
+
+// let toss = Math.floor( Math.random() * 2 ) +1;
+
+
+// if(toss===2){
+
+//     document.write(toss+"<br>"+"Random toss value heads")
+// }else{
+//     document.write(toss+"<br>"+"Random toss value tails")}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 6
+
+// let ranNum = Math.ceil( Math.random() * 100 ) +1;
+// document.write('Random number 1 to 100: ' + ranNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 7
+
+// let weight =parseInt(prompt("enter your weight in kilograms"));
+
+// document.write("The weight of user is "+weight+"kilograms");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 8
+
+// let guessNumber = prompt("enter secret number");
+
+// let ran = Math.ceil(Math.random()*10)+1;
+
+// if(guessNumber == ran){
+//     alert("congratulations you gues the right number "+ran)
+// }else{
+//     alert("try again the number is "+ran);
+// }
